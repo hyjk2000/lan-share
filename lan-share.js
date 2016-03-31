967530fa72c2aaf7a92f71968b67a29eb25454a6
James Shih <gtx1024@gmail.com>
2016-03-31 14:15:59 +0800

    Remove Express dependency

--- lan-share.js
+++ lan-share.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-var express    = require('express');
-var serveIndex = require('serve-index');
-var localIp    = require('./local-ip');
+var finalhandler = require('finalhandler');
+var http         = require('http');
+var localIp      = require('./local-ip');
+var serveIndex   = require('serve-index');
+var serveStatic  = require('serve-static');
 
-var app  = express();
 var path = process.argv[2] || process.env['PWD'];
 var port = process.argv[3] || 9527;
 
@@ -13,6 +14,14 @@ localIp().forEach(ip => {
   console.log(`${ip.name}\thttp://${ip.address}:${port}`);
 });
 
-app.use(serveIndex(path, {'icons': true, 'view': 'details'}));
-app.use(express.static(path));
-app.listen(port);
+var index = serveIndex(path, {'icons': true, 'view': 'details'});
+
+var serve = serveStatic(path);
+
+http.createServer(function onRequest(req, res) {
+  var done = finalhandler(req, res);
+  serve(req, res, function onNext(err) {
+    if (err) return done(err);
+    index(req, res, done);
+  });
+}).listen(port);
